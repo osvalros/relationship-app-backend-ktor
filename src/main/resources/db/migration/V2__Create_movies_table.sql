e7ab259ec5f4a5ac77b140e600b524d08a6dacbf
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-22 21:46:36 +0100

    Add migration to create movies table

---
+++ src/main/resources/db/migration/V2__Create_movies_table.sql
@@ -0,0 +1,11 @@
+create table movies
+(
+    id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name       VARCHAR   NOT NULL UNIQUE,
+    created_at timestamp NOT NULL,
+    viewed_at  timestamp,
+    creator_id INT,
+    CONSTRAINT fk_creator
+        FOREIGN KEY (creator_id)
+            REFERENCES users (id)
+);
