1230fa11302430a388f9a113c0806a495b7bd4c6
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-22 22:42:00 +0100

    Add forgotten default value for movies created_at

--- src/main/resources/db/migration/V2__Create_movies_table.sql
+++ src/main/resources/db/migration/V2__Create_movies_table.sql
@@ -2,7 +2,7 @@ create table movies
 (
     id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name       VARCHAR   NOT NULL UNIQUE,
-    created_at timestamp NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
     viewed_at  timestamp,
     creator_id INT,
     CONSTRAINT fk_creator
